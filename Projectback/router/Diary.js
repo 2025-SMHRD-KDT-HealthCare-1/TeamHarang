2ff6c81cf0c5bed4b0c6cb2f7e99d76c801b64a1
JHN <hni000003@gmail.com>
2025-12-02 14:25:33 +0900

    다이어리

--- Projectback/router/Diary.js
+++ Projectback/router/Diary.js
@@ -29,13 +29,56 @@ router.post('/AddDiary', (req,res) => {
         })
     }])
 })
+// 월 일기 작성여부 조회
+router.post('/MonthDiary', (req, res)=>{
+    const { uid, year, month } = req.body
+
+    if(!uid || !year || !month){
+        return res.status(400).json({message : "uid or 날짜 누락"})
+    }
+
+    const monthStr = month.toString().padStart(2, '0') // 한자리 월 일때 두자리로 맞춤
+    
+    // yyyy-mm으로 검색
+    const yearMonth = `${year}-${monthStr}`
+
+    const sql = `
+        select distinct date
+        from diary
+        where user_id = ? 
+            and date_format(date, %Y-%m) = ?
+        order by date ASC`
+
+        conn.query(sql, [uid, yearMonth], (err, result)=>{
+            if(err){
+                return res.status(500).json({message : "월별 일기 조회 실패"})
+            }
+
+            // 날짜만 배열로 추출
+            const dates = result.map(row => row.date)
+
+            return res.json({
+                message : "월별 작성 일기 날짜 조회 성공",
+                year,
+                month,
+                dates
+            })
+        })
+    })
 
 // 특정 날짜 다이어리 조회
 router.post('/GetDiaryDate', (req, res)=>{
     const { uid, date } = req.body
 
-    if(!uid || !date){
-        return res.status(400).json({message : "uid or date 누락"})
+    if(!uid){
+        return res.status(400).json({message : "uid 누락"})
+    }
+
+    // date가 없으면 오늘 날짜로 자동설정
+    let targetDate = date
+    if(!targetDate){
+        const today = new Date()
+        targetDate = today.toISOString().split('T')[0] //yyyy-mm-dd
     }
 
     const sql = `
@@ -45,17 +88,27 @@ router.post('/GetDiaryDate', (req, res)=>{
         order by diary_id asc
         `
 
-        conn.query(sql, [uid, date], (err, result)=>{
+        conn.query(sql, [uid, targetDate], (err, result)=>{
             if(err){
                 return res.status(500).json({message : "일기 조회 실패"})
             }
 
-            return res.json({
+            // 일기가 없는 경우
+            if(result.length === 0){
+                return res.json({
                 message : "특정 날짜 일기 조회 성공",
-                date : date,
-                diaries : result
+                date : targetDate,
+                info : "작성안함"
             })
+        }
+
+        // 일기가 있을 경우
+        return res.json({
+            message : "특정 날짜 일기 조회 성공",
+            date : targetDate,
+            diaries : result
         })
+    })
 })
 
 // 특정 다이어리 삭제
